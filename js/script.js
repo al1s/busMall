9b9d035315c1f86dbc665df5ee02cc121403da48
alstof <alstof@gmail.com>
2018-08-13 13:22:38 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+// show random three pictures on the page;
+// keep click results for each picture;
+// after 25 clicks:
+//   removeEventListeners;
+//   show results;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Project
+
+User story:...
